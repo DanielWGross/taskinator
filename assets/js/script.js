70b8ea3107492cf15d349c3735d99af63c1a91a0
Daniel Gross <danwgross@gmail.com>
2020-08-25 13:30:54 -0400

    4.2.6 capture form field values

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,18 @@ const tasksToDoEl = document.querySelector("#tasks-to-do");
 
 const createTaskHandler = (event) => {
   event.preventDefault();
-  const taskItemEl = document.createElement("li");
-  taskItemEl.textContent = "Hello";
-  taskItemEl.classList.add("task-item");
-  tasksToDoEl.appendChild(taskItemEl);
+  const taskNameInput = document.querySelector("input[name='task-name']");
+  const taskTypeInput = document.querySelector("select[name='task-type']");
+
+  const listItemEl = document.createElement("li");
+  listItemEl.classList.add("task-item");
+
+  const taskInfoEl = document.createElement("div");
+  taskInfoEl.classList.add("task-info");
+  taskInfoEl.innerHTML = `<h3 class='task-name'>${taskNameInput.value}</h3><span class='task-type'>${taskTypeInput.value}</span>`;
+
+  listItemEl.appendChild(taskInfoEl);
+  tasksToDoEl.appendChild(listItemEl);
 };
 
 formEl.addEventListener("submit", createTaskHandler);
